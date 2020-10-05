c93198a246d9cd6f45c3eedf586f748768d05356
xpextro93 <jonathanandrade@pursuit.org>
2020-10-05 19:31:16 -0400

    add webcam stream

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React, { useRef, useEffect } from 'react';
-import useScript from "./useScript.js"
-import faceapi from "./face-api.min.js";
+import React, { useState, useRef, useEffect } from 'react';
+import * as faceapi from "face-api.js";
 import './App.css';
 
 
 function App() {
-  // const status = useScript(
-  //   './face-api.min.js'
-  // );
-
+  const [ isReady, setIsReady ] = useState(false);
   const video = useRef(null);
+  const canvas = useRef(null);
    useEffect(() => {
-    console.log('this is happening')
-    navigator.getUserMedia(
+    const loadModels = async () =>{
+      const MODEL_URL = process.env.PUBLIC_URL + "models";
+      setIsReady(!isReady);
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
+      ]).then(start);
+    }
+    loadModels();
+   
+  },[])
+
+  const start = () => {
+     navigator.getUserMedia(
      { video:{}},
       steam => video.current.srcObject = steam,
       err => console.error(err)
     )
-  })
+  }
 
   return (
     <div className="App">
-      <div>
-        Script status: <b>{status}</b>
-      </div>
-      {status === "ready" ? 
-      <div> 
-         <video width= "720" height="560" autoPlay muted ref={video}>
-         </video>
-
-
-        
-      </div> :<div>Its not ready</div>}
+      <h1>{isReady? "ready" : "not ready"}</h1>
+      <video width= "720" height="560" autoPlay muted ref={video}/>
     </div>
   );
 
