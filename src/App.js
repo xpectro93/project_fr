40a4036a991d5b99ee269a9ed3367f206f517d01
xpextro93 <jonathanandrade@pursuit.org>
2020-10-06 13:44:17 -0400

    add first working version of working face api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import * as faceapi from "face-api.js";
 import './App.css';
+import { detectAllFaces } from 'face-api.js';
 
 
 function App() {
@@ -29,11 +30,37 @@ function App() {
       err => console.error(err)
     )
   }
+  const play = () => {
+    setInterval(async() => {
+      if(isReady) {
+        setIsReady(false);
+      }
+      canvas.current.innerHTML = faceapi.createCanvasFromMedia(video.current);
+
+      const display = {
+        width:720,
+        height:560
+      }
+      faceapi.matchDimensions(canvas.current, display);
+      const data = await detectAllFaces(video.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+
+      const resize = faceapi.resizeResults(data, display);
+
+      canvas.current.getContext("2d").clearRect(0,0, display.width, display.height);
+
+      faceapi.draw.drawDetections(canvas.current, resize);
+      faceapi.draw.drawFaceLandmarks(canvas.current, resize);
+      faceapi.draw.drawFaceExpressions(canvas.current, resize);
+
+        console.log(data)
+    },100)
+  }
 
   return (
     <div className="App">
       <h1>{isReady? "ready" : "not ready"}</h1>
-      <video width= "720" height="560" autoPlay muted ref={video}/>
+      <video width= "720" height="560" autoPlay muted ref={video} onPlay={play}/>
+      <canvas width= "720" height="560" ref={canvas}/>
     </div>
   );
 
