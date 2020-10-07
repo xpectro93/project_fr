07a715eb5d66505bb71075f39b4b129c1fbb6cb0
xpextro93 <jonathanandrade@pursuit.org>
2020-10-07 15:13:57 -0400

    add ShowExpressions component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import * as faceapi from "face-api.js";
 import './App.css';
 import { detectAllFaces } from 'face-api.js';
 
+import ShowExpression from './ShowExpression.js'
 
 function App() {
   const [ isReady, setIsReady ] = useState(false);
+  const [ data, setData ] = useState(null);
+
   const video = useRef(null);
   const canvas = useRef(null);
    useEffect(() => {
@@ -51,15 +54,24 @@ function App() {
       faceapi.draw.drawDetections(canvas.current, resize);
       faceapi.draw.drawFaceLandmarks(canvas.current, resize);
       faceapi.draw.drawFaceExpressions(canvas.current, resize);
+      
+      if(data[0]) {
+        setData(data[0].expressions);
 
-        console.log(data)
-    },3000)
+      }
+    },300)
   }
 
   return (
     <div className="App">
-      <video width= "720" height="560" autoPlay muted ref={video} onPlay={play}/>
-      <canvas ref={canvas}/>
+        <div id="faceWrapper"> 
+          <video width= "720" height="560" autoPlay muted ref={video} onPlay={play}/>
+          <canvas ref={canvas}/>
+        </div>
+
+       
+
+      
     </div>
   );
 
---
+++ src/ShowExpression.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Showexpression({expressions}) {
+    
+    
+    return (
+        <>
+            <h1> expressions</h1>
+        </>
+    )
+}
