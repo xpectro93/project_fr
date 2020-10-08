963c91d14dec2b7dee13086165be7ea5077643ce
xpextro93 <jonathanandrade@pursuit.org>
2020-10-08 17:46:15 -0400

    show images from api

--- src/ShowExpression.js
+++ src/ShowExpression.js
@@ -1,14 +1,17 @@
-import React, { useEffect} from 'react';
+import React, { useEffect , useState } from 'react';
 import axios from  'axios'
 
 
 const url = 'https://www.reddit.com/r/eyebleach.json?&count=25'
 export default function Showexpression({expressions}) {
+    const [ resp, setResp ] = useState(null);
+
     useEffect(()=> {
         const fetchContent = async () => {
             try {
                 let response = await axios.get(url);
-                console.log("REDDIT",response)
+                console.log("REDDIT",response);
+                setResp(response)
             }
             catch {
     
@@ -40,8 +43,22 @@ export default function Showexpression({expressions}) {
         
 
     return (
-        <>
+        <div className="view">
             <h1>{topExpression.name}</h1>
-        </>
+            {resp ? <div>
+                        {resp.data.data.children.map(post => {
+                            if(post.data.url.includes('jpg')) 
+                            {
+                                return <img className="jpg" src={post.data.url}></img>
+                            }
+                            return <h1>{post.data.url}</h1>
+                        })
+                        }
+                   </div> : "no data"}
+        </div>
     )
 }
+                            // post.data.url.includes("gif") ||
+                            // post.data.url.includes("jpeg")||
+                            // post.data.url.includes("gif") ||
+                            // post.data.url.includes("gfycat")
