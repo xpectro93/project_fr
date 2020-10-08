bfa8ea56ba998aa3c15c25d64d4c6bb4ab43d505
xpextro93 <jonathanandrade@pursuit.org>
2020-10-08 16:02:42 -0400

    add reddit api test

--- src/ShowExpression.js
+++ src/ShowExpression.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect} from 'react';
+import axios from  'axios'
 
+
+const url = 'https://www.reddit.com/r/eyebleach.json?&count=25'
 export default function Showexpression({expressions}) {
+    useEffect(()=> {
+        const fetchContent = async () => {
+            try {
+                let response = await axios.get(url);
+                console.log("REDDIT",response)
+            }
+            catch {
     
+            }
+        }
+        fetchContent()
+    }, [])
+    
+
+
+
     let topExpression = {
         name:"Loading",
         value:0
@@ -17,6 +35,7 @@ export default function Showexpression({expressions}) {
             }
     
         }
+
 
         
 
